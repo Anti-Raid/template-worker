a72fd49b0019c1fc742ee1526e6d28055d966b04
Your Name <you@example.com>
2026-02-10 12:03:49 -0500

    begin improving db mapper

--- src/securelua/db.rs
+++ src/securelua/db.rs
@@ -1,11 +1,11 @@
-use khronos_runtime::rt::{mlua_scheduler::LuaSchedulerAsyncUserData, mluau::prelude::*};
+use khronos_runtime::{chrono_tz, core::{datetime::DateTime as LuaDateTime, typesext::I64}, rt::{mlua_scheduler::LuaSchedulerAsyncUserData, mluau::prelude::*}};
 use sqlx::PgPool;
 use chrono::{DateTime, Utc};
 use serde_json::Value as JsonValue;
 use sqlx::types::Uuid;
 
 macro_rules! db_index_map {
-    ($($type:ty => { $base:ident, $opt:ident, $list:ident, $typestr:literal }),* $(,)?) => {
+    ($($type:ty => { $base:ident, $opt:ident, $list:ident, $typestr:literal, |$lua:ident, $val:ident| $luaconv:block, |$luaf:ident, $opaque:ident| $luaconvf:block }),* $(,)?) => {
         use serde::{Serialize, Deserialize};
         use sqlx::Row;
 
@@ -41,19 +41,63 @@ macro_rules! db_index_map {
                 match type_name {
                     $(
                         $typestr => {
-                            let val: $type = lua.from_value(value)?;
+                            let func = |$lua: &Lua, $val: LuaValue| $luaconv;
+                            let val: $type = func(lua, value)?;
                             Ok(OpaqueValue::$base(val))
                         },
                         concat!($typestr, "?") => {
-                            let val: Option<$type> = lua.from_value(value)?;
-                            Ok(OpaqueValue::$opt(val))
+                            let func = |$lua: &Lua, $val: LuaValue| $luaconv;
+                            if let LuaValue::Nil = value {
+                                return Ok(OpaqueValue::$opt(None));
+                            }
+                            let val: $type = func(lua, value)?;
+                            Ok(OpaqueValue::$opt(Some(val)))
                         },
                         concat!("{", $typestr, "}") => {
-                            let val: Vec<$type> = lua.from_value(value)?;
-                            Ok(OpaqueValue::$list(val))
+                            let func = |$lua: &Lua, $val: LuaValue| $luaconv;
+                            match value {
+                                LuaValue::Table(table) => {
+                                    let mut vec = Vec::new();
+                                    table.for_each_value::<LuaValue>(|v| {
+                                        let val: $type = func(lua, v)?;
+                                        vec.push(val);
+                                        Ok(())
+                                    })?;
+                                    Ok(OpaqueValue::$list(vec))
+                                }
+                                _ => return Err(LuaError::external(format!("Expected a table for type {{}}: {}", $typestr))),
+                            }
                         },
                     )*
                     _ => Err(LuaError::external(format!("Unknown type for OpaqueValue conversion: {}", type_name))),
+                }
+            }
+
+            pub fn into_lua(self, lua: &Lua) -> LuaResult<LuaValue> {
+                match self {
+                    $(
+                        OpaqueValue::$base(v) => {
+                            let func = |$luaf: &Lua, $opaque: $type| $luaconvf;
+                            func(lua, v)
+                        },
+                        OpaqueValue::$opt(v) => {
+                            let func = |$luaf: &Lua, $opaque: $type| $luaconvf;
+                            let Some(v) = v else {
+                                return Ok(LuaValue::Nil);
+                            };
+                            func(lua, v)
+                        },
+                        OpaqueValue::$list(v) => {
+                            let func = |$luaf: &Lua, $opaque: $type| $luaconvf;
+                            let table = lua.create_table()?;
+                            for item in v {
+                                let lua_val = func(lua, item)?;
+                                table.push(lua_val)?;
+                            }
+                            table.set_readonly(true);
+                            Ok(LuaValue::Table(table))
+                        },
+                    )*
                 }
             }
 
@@ -137,13 +181,36 @@ macro_rules! db_index_map {
 }
 
 db_index_map! {
-    i32 => { I32, I32Opt, I32List, "i32" },
-    String => { String, StringOpt, StringList, "string" },
-    bool => { Bool, BoolOpt, BoolList, "boolean" },
-    f64 => { F64, F64Opt, F64List, "f64" },
-    DateTime<Utc> => { DateTime, DateTimeOpt, DateTimeList, "datetime" },
-    JsonValue => { Json, JsonOpt, JsonList, "json" },
-    Uuid => { Uuid, UuidOpt, UuidList, "uuid" },
+    i32 => { I32, I32Opt, I32List, "i32", |lua, value| { lua.from_value(value) }, |lua, opaque| { lua.to_value(&opaque) } },
+    i64 => { I64, I64Opt, I64List, "i64", |lua, value| { 
+        match value {
+            LuaValue::UserData(ud) => {
+                if let Ok(i64) = ud.borrow::<I64>() {
+                    Ok(i64.0)
+                } else {
+                    Err(LuaError::external("Expected I64 userdata"))
+                }
+            }
+            _ => lua.from_value(value)
+        }
+    }, |lua, opaque| { lua.to_value(&opaque) } },
+    String => { String, StringOpt, StringList, "string", |lua, value| { lua.from_value(value) }, |lua, opaque| { lua.to_value(&opaque) } },
+    bool => { Bool, BoolOpt, BoolList, "boolean", |lua, value| { lua.from_value(value) }, |_lua, opaque| { Ok::<_, LuaError>(LuaValue::Boolean(opaque)) } },
+    f64 => { F64, F64Opt, F64List, "f64", |lua, value| { lua.from_value(value) }, |lua, opaque| { lua.to_value(&opaque) } },
+    DateTime<Utc> => { DateTime, DateTimeOpt, DateTimeList, "datetime", |lua, value| { 
+        match value {
+            LuaValue::UserData(s) => {
+                if let Ok(dt) = s.borrow::<LuaDateTime<chrono_tz::Tz>>() {
+                    Ok(dt.dt.with_timezone(&chrono::Utc))
+                } else {
+                    Err(LuaError::external("Expected DateTime<Utc> userdata"))
+                }
+            }
+            _ => lua.from_value(value)
+        }    
+    }, |lua, opaque| { lua.to_value(&opaque) } },
+    JsonValue => { Json, JsonOpt, JsonList, "json", |lua, value| { lua.from_value(value) }, |lua, opaque| { lua.to_value(&opaque) } },
+    Uuid => { Uuid, UuidOpt, UuidList, "uuid", |lua, value| { lua.from_value(value) }, |lua, opaque| { lua.to_value(&opaque) } },
 }
 
 #[allow(dead_code)]
@@ -160,7 +227,7 @@ impl LuaUserData for Db {
             }
         });
 
-        methods.add_method("cast", |lua, _this, (value, typ): (LuaValue, String)| {
+        methods.add_method("cast", |lua, _this: &Db, (value, typ): (LuaValue, String)| {
             OpaqueValue::from_lua(lua, value, &typ)
         });
 
