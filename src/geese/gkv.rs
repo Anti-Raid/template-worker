d8cd7536203fd27ad6c0f08bb2656b7b644b632d
Jack Tanner <jtanner0786@gmail.com>
2026-03-26 23:54:30 -0400

    GKV api

--- src/geese/gkv.rs
+++ src/geese/gkv.rs
@@ -91,65 +91,6 @@ impl GlobalKeyValueDb {
 
         Ok(Some(gkv))
     }
-
-    pub async fn global_kv_create(&self, id: Id, gkv: CreateGlobalKv) -> Result<(), crate::Error> {
-        // Validate key
-        //
-        // Rules:
-        // 1. Between 3 and 64 characters long
-        // 2. May not start or end with a dot (.)
-        // 3. May only contain (ASCII) alphanumeric characters, dots (.), dashes (-), and underscores (_)
-        if gkv.key.len() < 3 || gkv.key.len() > 64 {
-            return Err("keys must be between 3 and 64 characters long".into());
-        }
-        if gkv.key.starts_with('.') || gkv.key.ends_with('.') {
-            return Err("keys may not start or end with a dot".into());
-        }
-        if !gkv.key.chars().all(|c| c.is_ascii_alphanumeric() || c == '.' || c == '-' || c == '_') {
-            return Err("keys may only contain alphanumeric characters, dots, dashes, and underscores".into());
-        }
-
-        let inserted = sqlx::query(
-            "INSERT INTO global_kv (key, version, owner_id, owner_type, short, long, public_metadata, public_data, scope, data) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) ON CONFLICT (key, version, scope) DO NOTHING",
-        )
-        .bind(&gkv.key)
-        .bind(gkv.version)
-        .bind(id.tenant_id())
-        .bind(id.tenant_type())
-        .bind(&gkv.short)
-        .bind(&gkv.long)
-        .bind(serde_json::to_value(gkv.public_metadata)?)
-        .bind(gkv.public_data)
-        .bind(&gkv.scope)
-        .bind(serde_json::to_value(gkv.data)?)
-        .execute(&self.pool)
-        .await?;
-
-        if inserted.rows_affected() == 0 {
-            return Err("Global KV with the same key, version, and scope already exists".into());
-        }
-
-        Ok(())
-    }
-
-    pub async fn global_kv_delete(&self, id: Id, key: String, version: i32, scope: String) -> Result<(), crate::Error> {
-        let res = sqlx::query(
-        "DELETE FROM global_kv WHERE key = $1 AND version = $2 AND scope = $3 AND owner_id = $4 AND owner_type = $5",
-        )
-        .bind(key)
-        .bind(version)
-        .bind(scope)
-        .bind(id.tenant_id())
-        .bind(id.tenant_type())
-        .execute(&self.pool)
-        .await?;
-
-        if res.rows_affected() == 0 {
-            return Err("No matching Global KV found to delete or insufficient permissions".into());
-        }
-
-        Ok(())
-    }
 }
 
 #[derive(Debug, Serialize, Deserialize, TS, utoipa::ToSchema, sqlx::FromRow)]
@@ -226,25 +167,4 @@ impl Default for GlobalKvData {
             opaque: true,
         }
     }
-}
-
-/// NOTE: Global KV's created publicly cannot have a price associated to them for legal reasons.
-/// Only staff may create priced global KV's.
-/// NOTE 2: All Global KV's undergo staff review before being made available. When this occurs,
-/// review state will be updated accordingly from 'pending' to 'approved' or otherwise if rejected.
-#[derive(Debug, Serialize, Deserialize, TS, utoipa::ToSchema, sqlx::FromRow)]
-#[ts(export)]
-pub struct CreateGlobalKv {
-    pub key: String,
-    pub version: i32,
-    pub short: String, // short description for the key-value.
-    #[ts(as = "KhronosValueApi")]
-    #[schema(value_type = KhronosValueApi)]
-    pub public_metadata: KhronosValue, // public metadata about the key-value
-    pub scope: String,
-    pub public_data: bool,
-    pub long: Option<String>, // long description for the key-value.
-    #[ts(as = "KhronosValueApi")]
-    #[schema(value_type = KhronosValueApi)]
-    pub data: KhronosValue, // the actual value of the key-value, may be private
 }
--- src/geese/state.rs
+++ src/geese/state.rs
@@ -1,3 +1,4 @@
+use chrono::{DateTime, Utc};
 use khronos_runtime::rt::mlua::prelude::*;
 use khronos_runtime::utils::khronos_value::KhronosValue;
 use rand::distr::{Alphanumeric, SampleString};
@@ -21,6 +22,30 @@ pub enum StateOp {
     },
     KvDelete {
         key: String,
+        scope: String
+    },
+    GlobalKvFind {
+        query: String,
+        scope: String
+    },
+    GlobalKvGet {
+        key: String,
+        version: i32,
+        scope: String
+    },
+    GlobalKvCreate {
+        key: String,
+        version: i32,
+        short: String, // short description for the key-value.
+        public_metadata: KhronosValue, // public metadata about the key-value
+        scope: String,
+        public_data: bool,
+        long: Option<String>, // long description for the key-value.
+        data: KhronosValue, // the actual value of the key-value, may be private
+    },
+    GlobalKvDelete {
+        key: String,
+        version: i32,
         scope: String
     },
     UpdateTenantState {
@@ -66,6 +91,34 @@ impl FromLua for StateOp {
                 let events = tab.get("events")?;
                 let flags = tab.get("flag")?;
                 Ok(Self::UpdateTenantState { events, flags })
+            },
+            "GlobalKvFind" => {
+                let query = tab.get("query")?;
+                let scope = tab.get("scope")?;
+                Ok(Self::GlobalKvFind { query, scope })
+            },
+            "GlobalKvGet" => {
+                let key = tab.get("key")?;
+                let version = tab.get("version")?;
+                let scope = tab.get("scope")?;
+                Ok(Self::GlobalKvGet { key, version, scope })
+            },
+            "GlobalKvCreate" => {
+                let key = tab.get("key")?;
+                let version = tab.get("version")?;
+                let short = tab.get("short")?;
+                let public_metadata = tab.get("public_metadata")?;
+                let scope = tab.get("scope")?;
+                let public_data = tab.get("public_data")?;
+                let long = tab.get("long").ok();
+                let data = tab.get("data")?;
+                Ok(Self::GlobalKvCreate { key, version, short, public_metadata, scope, public_data, long, data })
+            },
+            "GlobalKvDelete" => {
+                let key = tab.get("key")?;
+                let version = tab.get("version")?;
+                let scope = tab.get("scope")?;
+                Ok(Self::GlobalKvDelete { key, version, scope })
             },
             _ => {
                 Err(LuaError::FromLuaConversionError {
@@ -203,6 +256,96 @@ impl StateDb {
 
                 state.new_tenant_state = Some((events, flags));
             }
+            StateOp::GlobalKvFind { query, scope } => {
+                let items: Vec<PartialGlobalKv> = if query == "%%" {
+                    sqlx::query_as(
+                        "SELECT key, version, owner_id, owner_type, short, public_metadata, public_data, scope, created_at, last_updated_at, price, review_state FROM global_kv WHERE scope = $1 AND review_state = 'approved'"
+                    )
+                    .bind(scope)
+                    .fetch_all(executor)
+                    .await?
+                } else {
+                    sqlx::query_as(
+                        "SELECT key, version, owner_id, owner_type, short, public_metadata, public_data, scope, created_at, last_updated_at, price, review_state FROM global_kv WHERE scope = $1 AND review_state = 'approved' AND key LIKE $2"
+                    )
+                    .bind(scope)
+                    .bind(query)
+                    .fetch_all(executor)
+                    .await?
+                };
+
+                PartialGlobalKv::apply(state, items);
+            }
+            StateOp::GlobalKvGet { key, version, scope } => {
+                if let Some(rec) = sqlx::query_as(
+                    "SELECT key, version, owner_id, owner_type, short, public_metadata, public_data, scope, created_at, last_updated_at, price, review_state FROM global_kv WHERE key = $1 AND version = $2 AND scope = $3 AND review_state = 'approved'",
+                    )
+                    .bind(&key)
+                    .bind(version)
+                    .bind(scope)
+                    .fetch_optional(executor)
+                    .await? {
+                        PartialGlobalKv::apply_one(state, rec);
+                    }
+            }
+            StateOp::GlobalKvCreate { key, version, short, public_metadata, scope, public_data, long, data } => {
+                // Validate key
+                //
+                // Rules:
+                // 1. Between 3 and 64 characters long
+                // 2. May not start or end with a dot (.)
+                // 3. May only contain (ASCII) alphanumeric characters, dots (.), dashes (-), and underscores (_)
+                if key.len() < 3 || key.len() > 64 {
+                    return Err("keys must be between 3 and 64 characters long".into());
+                }
+                if key.starts_with('.') || key.ends_with('.') {
+                    return Err("keys may not start or end with a dot".into());
+                }
+                if !key.chars().all(|c| c.is_ascii_alphanumeric() || c == '.' || c == '-' || c == '_') {
+                    return Err("keys may only contain alphanumeric characters, dots, dashes, and underscores".into());
+                }
+                
+                let id = Alphanumeric.sample_string(&mut rand::rng(), 64);
+                
+                // try to insert, if it fails then a record with the same key, version, and scope already exists
+                // so we can return an error
+                let inserted = sqlx::query(
+                    "INSERT INTO global_kv (id, key, version, owner_id, owner_type, short, public_metadata, public_data, scope, long, data) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) ON CONFLICT (key, version, scope) DO NOTHING"
+                )
+                .bind(&id)
+                .bind(key)
+                .bind(version)
+                .bind(tid.tenant_id())
+                .bind(tid.tenant_type())
+                .bind(short)
+                .bind(serde_json::to_value(public_metadata)?)
+                .bind(public_data)
+                .bind(scope)
+                .bind(long)
+                .bind(serde_json::to_value(data)?)
+                .execute(executor)
+                .await?;
+
+                if inserted.rows_affected() == 0 {
+                    return Err("Global KV with the same key, version, and scope already exists".into());
+                }
+            }
+            StateOp::GlobalKvDelete { key, version, scope } => {
+                let res = sqlx::query(
+        "DELETE FROM global_kv WHERE key = $1 AND version = $2 AND scope = $3 AND owner_id = $4 AND owner_type = $5",
+                )
+                .bind(key)
+                .bind(version)
+                .bind(scope)
+                .bind(tid.tenant_id())
+                .bind(tid.tenant_type())
+                .execute(executor)
+                .await?;
+
+                if res.rows_affected() == 0 {
+                    return Err("No matching Global KV found to delete or insufficient permissions".into());
+                }
+            }
         }
 
         Ok(())
@@ -248,3 +391,51 @@ impl KvLookup {
         }
     }
 }
+
+#[derive(Debug, sqlx::FromRow)]
+pub struct PartialGlobalKv {
+    pub key: String,
+    pub version: i32,
+    pub owner_id: String,
+    pub owner_type: String,
+    pub price: Option<i64>, // will only be set for shop items, otherwise None
+    pub short: String, // short description for the key-value.
+    #[sqlx(json)]
+    pub public_metadata: KhronosValue, // public metadata about the key-value
+    pub scope: String,
+    pub created_at: DateTime<Utc>,
+    pub last_updated_at: DateTime<Utc>,
+    pub public_data: bool,
+    pub review_state: String,
+
+    #[sqlx(default)]
+    pub long: Option<String>, // long description for the key-value.
+}
+
+impl PartialGlobalKv { 
+    fn apply_one(state: &mut StateExecResponse, l: PartialGlobalKv) {
+        state.results.push(StateExecResult {
+            key: l.key,
+            scope: l.scope,
+            value: KhronosValue::Map(vec![
+                (KhronosValue::Text("version".to_string()), KhronosValue::Integer(l.version as i64)),
+                (KhronosValue::Text("owner_id".to_string()), KhronosValue::Text(l.owner_id)),
+                (KhronosValue::Text("owner_type".to_string()), KhronosValue::Text(l.owner_type)),
+                (KhronosValue::Text("short".to_string()), KhronosValue::Text(l.short)),
+                (KhronosValue::Text("public_metadata".to_string()), l.public_metadata),
+                (KhronosValue::Text("public_data".to_string()), KhronosValue::Boolean(l.public_data)),
+                (KhronosValue::Text("review_state".to_string()), KhronosValue::Text(l.review_state)),
+                (KhronosValue::Text("long".to_string()), l.long.map_or(KhronosValue::Null, |s| KhronosValue::Text(s))),
+                (KhronosValue::Text("price".to_string()), l.price.map_or(KhronosValue::Null, |p| KhronosValue::Integer(p))),
+            ]),
+            created_at: Some(l.created_at),
+            last_updated_at: Some(l.last_updated_at),
+        })
+    }
+
+    fn apply(state: &mut StateExecResponse, lookups: Vec<PartialGlobalKv>) {
+        for l in lookups {
+            Self::apply_one(state, l);
+        }
+    }
+}   
