e06494063f166f24193cce5e6725a9792ef5d931
Your Name <you@example.com>
2026-07-12 10:08:11 +0530

    improve error boxes, fix builds

--- devutils/willow/src/lib/ErrorBox.svelte
+++ devutils/willow/src/lib/ErrorBox.svelte
@@ -20,8 +20,6 @@
       <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
     </svg>
     <span class="sr-only">Error</span>
-    <div>
-      {message}
-    </div>
+    <code class="whitespace-pre-wrap font-sans text-md">{message}</code>
   </div>
 {/if}
